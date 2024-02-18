9f562475ca8143ba5703143afcc34d92af8cb5ee
Nazi <mehdizadeh.sn@gmail.com>
2024-02-18 20:35:45 +0000

    in function body a Zombie created and added to the array

--- Solidity/cryptoZombie/ZombieFactory.sol
+++ Solidity/cryptoZombie/ZombieFactory.sol
@@ -13,7 +13,7 @@ contract ZombieFactory {
     Zombie[] public zmobies;
 
     function createZombie( string memory _name, uint _dna) public {
-        
+        Zombie.push(Zombie(_name, _dna));
     }
 
     
