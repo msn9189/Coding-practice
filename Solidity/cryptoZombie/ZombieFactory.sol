f5c9c8821f9f076758c1cdf2c99d2c43baab869f
Nazi <mehdizadeh.sn@gmail.com>
2024-02-29 20:24:07 +0000

    NewZombie event created

--- Solidity/cryptoZombie/ZombieFactory.sol
+++ Solidity/cryptoZombie/ZombieFactory.sol
@@ -2,6 +2,8 @@ pragma solidity >=0.5.0 <0.6.0;
 
 contract ZombieFactory {
 
+    event NewZombie(uint zombieId, string name, uint dna);
+
     uint dnaDigits = 16;
     uint dnaModulus = 10 ** dnaDigits;
 
