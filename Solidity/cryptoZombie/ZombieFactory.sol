8dfecd7b3ea86c447691101767d1aa2a647bd3de
Nazi <mehdizadeh.sn@gmail.com>
2024-02-29 20:12:55 +0000

    the random function completed

--- Solidity/cryptoZombie/ZombieFactory.sol
+++ Solidity/cryptoZombie/ZombieFactory.sol
@@ -18,6 +18,8 @@ contract ZombieFactory {
 
     
     function _generateRandomDna(string memory _str) private view returns (uint) {
-    
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+        
     }
 }
