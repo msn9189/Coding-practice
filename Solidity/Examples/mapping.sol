327e2b8b85b06022c1d45a61460b67965dd5dc63
Nazi <mehdizadeh.sn@gmail.com>
2024-11-28 20:52:35 +0000

    get function created

--- Solidity/Examples/mapping.sol
+++ Solidity/Examples/mapping.sol
@@ -3,5 +3,9 @@ pragma solidity 0.8.26;
 
 contract Mapping {
     mapping(address => uint256) public myMap;
+
+    function get(address _addr) public view returns (uint256) {
+        return myMap[_addr];
+    }
     
 }
