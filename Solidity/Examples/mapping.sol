94eb948b2dbff57df20f1305d392c793bd05987f
MSN <mehdizadeh.sn@gmail.com>
2024-11-29 21:30:47 +0000

    Comment added to the Mapping contract

--- Solidity/Examples/mapping.sol
+++ Solidity/Examples/mapping.sol
@@ -2,17 +2,22 @@
 pragma solidity 0.8.26;
 
 contract Mapping {
+    //Mapping from address to uint
     mapping(address => uint256) public myMap;
 
     function get(address _addr) public view returns (uint256) {
+        // Mapping always returns a value
+        // If the value was never set, it will return the default value.
         return myMap[_addr];
     }
 
     function set(address _addr, uint56 _i) public {
+        // Update the value at this address
         myMap[_addr] = _i;
     }
 
     function remove(address _addr) public {
+        // Reset the value to the default value.
         delete myMap[_addr];
     }
 }
