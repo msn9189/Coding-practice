6567dfaf09cae29a85130d64dad705167be4a266
Nazi <mehdizadeh.sn@gmail.com>
2024-11-30 11:51:20 +0000

    a set function defined for recording the nested mapping

--- Solidity/Examples/mapping.sol
+++ Solidity/Examples/mapping.sol
@@ -28,4 +28,8 @@ contract NestedMapping {
     function get(address _addr1, uint256 _i) public view returns (bool) {
         return nested[_addr1][_i];
     }
+
+    function set(address _addr1, uint256 _i, bool _boo) public {
+        nested[_addr1][_i] = _boo;
+    }
 }
