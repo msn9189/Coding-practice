46a7656b80c8948800e512af3fd90a3b4f979a97
Nazi <mehdizadeh.sn@gmail.com>
2024-11-30 11:53:10 +0000

    remove function defined

--- Solidity/Examples/mapping.sol
+++ Solidity/Examples/mapping.sol
@@ -32,4 +32,8 @@ contract NestedMapping {
     function set(address _addr1, uint256 _i, bool _boo) public {
         nested[_addr1][_i] = _boo;
     }
+
+    function remove(address _addr1, uint256 _i) public {
+        delete nested[_addr1][_i];
+    }
 }
