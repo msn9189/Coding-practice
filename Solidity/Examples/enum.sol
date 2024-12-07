af285cc705e68c8050a62d550a2a3e07660cb6a5
Nazi <mehdizadeh.sn@gmail.com>
2024-12-07 11:55:29 +0000

    an Enum variable defined

--- Solidity/Examples/enum.sol
+++ Solidity/Examples/enum.sol
@@ -2,5 +2,14 @@
 pragma solidity 0.8.26;
 
 contract Enum {
+    enum Status {
+        Pending,
+        Shipped,
+        Accepted,
+        Rejected,
+        Canceled
+    }
+
+    Status public status;
     
 }
