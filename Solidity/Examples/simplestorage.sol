bb3294db5e61515a5587e8b2ff02e958790e1424
MSN <mehdizadeh.sn@gmail.com>
2025-04-01 12:36:34 +0000

    Update comments

--- Solidity/Examples/simplestorage.sol
+++ Solidity/Examples/simplestorage.sol
@@ -2,11 +2,15 @@
 pragma solidity ^0.8.26;
 
 contract SimpleStorage {
+    //starte variable to store a number
     uint256 public num;
 
+    //You need to send a transaction to write to a state variable
     function set(uint256 _num) public {
         num = _num;
     }
+
+    //You can read from a state variable without sending a transaction
     function get() public view returns (uint256) {
         return num;
     }
