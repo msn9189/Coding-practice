dc3a10a14785cc288a444446c19d374c08858d0f
Nazi <mehdizadeh.sn@gmail.com>
2025-04-26 06:27:00 +0000

    ClassInheritance file created for practicing class in JS

--- Javascript/errorPrevention.js
+++ Javascript/errorPrevention.js
@@ -1,17 +1,16 @@
-function addTwoNums(a, b){
-    try{
-        if(typeof(a) != 'number'){
-            throw new TypeError('The first argument is not a number.');
-        }else if(typeof(b) != "number"){
-            throw new TypeError("The second argument is not a number.");
-        } else{
-            console.log(a + b);
-        }
-        
-    }catch(err){
-        console.log("Error!", err);
+function addTwoNums(a, b) {
+  try {
+    if (typeof a != "number") {
+      throw new TypeError("The first argument is not a number.");
+    } else if (typeof b != "number") {
+      throw new TypeError("The second argument is not a number.");
+    } else {
+      console.log(a + b);
     }
+  } catch (err) {
+    console.log("Error!", err);
+  }
 }
 
-addTwoNums('2', 2);
-console.log("It still works.")
+addTwoNums("2", 2);
+console.log("It still works.");
